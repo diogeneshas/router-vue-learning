8ca9afa2c2f7cca61cf5239e7e13f2dbbf65ea43
diogeneshas <diogenes.henrique.br@gmail.com>
2023-04-28 08:02:18 -0300

    programmatically adjust the route

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -4,15 +4,18 @@
     <p>{{car.name}}</p>
     <p>{{car.year}}</p>
     <p>{{car.price}}</p>
+    <button @click="router.push(`/cars/${carId}/contact`)">Click for contact</button>
     <RouterView />
   </div>
 </template>
 
 <script setup>
-import { useRoute, RouterView } from "vue-router"
+import { useRoute, RouterView, useRouter } from "vue-router"
 import cars from "../data/cars.json"
 
 const route = useRoute()
+const router = useRouter()
 const car = cars.find(c => c.id === parseInt(route.params.id))
+const carId = parseInt(route.params.id)
 
 </script>
