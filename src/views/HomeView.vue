3aaad33f59e13c2102e3426a79659974619fda49
diogeneshas <diogenes.henrique.br@gmail.com>
2023-04-28 07:26:32 -0300

    Pages with dynamic Paths

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
     <h1>Home View</h1>
+    <div class="cars">
+      <RouterLink :to="`/cars/${car.id}`" v-for="car in cars" :key="car.id">{{ car.name }}</RouterLink>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { RouterLink } from "vue-router"
+import cars from "../data/cars.json"
+</script>
+
+
+<style>
+.cars {
+  display: flex;
+  background: white;
+}
+
+.cars a {
+  text-decoration: none;
+  color: black;
+  width: 100%;
+  box-shadow: 1px 1px 10px rgba(50, 0, 0, 0.388);
+  margin: 10px;
+}
+</style>
